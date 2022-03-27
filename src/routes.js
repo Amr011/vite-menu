d242dbe9943fd352e3d9020fc510fd557dace000
Amr109 <grand.109.master@gmail.com>
2022-03-27 12:43:16 +0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('./views/Home.vue'),
+    meta: { title: 'Home' },
+  },
+  {
+    path: '/about',
+    meta: { title: 'About' },
+    component: () => import('./views/About.vue'),
+  },
+  {
+    path: '/:path(.*)',
+    component: () => import('./views/NotFound.vue'),
+  },
+]
+export default routes
